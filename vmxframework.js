2171d57662f0a925ecec9a00b5beb5f58f0884d0
Geoff Golder <gdoteof@gmail.com>
2015-01-22 17:17:34 -0500

    adding ready flag

--- vmxframework.js
+++ vmxframework.js
@@ -1,5 +1,6 @@
 var $vmx = {};
 $(document).ready(function(){
+  $vmx.ready = false;
   var injector = angular.element(document).injector();
   var vmxconnections = injector.get('vmxconnections');
   $vmx.models = injector.get('vmxmodels');
@@ -7,13 +8,9 @@ $(document).ready(function(){
   $vmx.detectorFactory = injector.get('VmxDetectorProviderX');
   $vmx.imageStreamProvider     = injector.get('vmxImageStreamProvider');
   $vmx.defaultStream = $vmx.imageStreamProvider.getInstance(document.getElementById('video-vmx-vmx'));
-
   $vmx.appcode = injector.get('vmxappcode');
   $vmx.defaultDetector = $vmx.detectorFactory.getInstance();
   $vmx.defaultDetector.setVideoSrc($vmx.defaultStream);
   $vmx.detect = $vmx.defaultDetector.detect
+  $vmx.ready = true;
 });
-
-
-
-
